33d597a27f0babaa5ed66b88afe931168404a04b
Luz Alba Posse <luzalbaposse@gmail.com>
2023-08-29 20:52:50 -0300

    little enigma

--- main.c
+++ main.c
@@ -66,36 +66,193 @@ int main(){
 
     // littleEnigma
     printf("littleEnigmaDecrypt y Encrypt\n");
-    char* alphabetPermutation[2];
+    // char* alphabetPermutation[2];
+    // alphabetPermutation[0] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+    // alphabetPermutation[1] = "NTZPSFBOKMWRCJDIVLAEYUXHGQ";
+    // struct littleEnigma* le = littleEnigmaNew(alphabetPermutation, 2);
+    // littleEnigmaPrint(le);
+    // printf("\n\n");
+
+    // int password[2] = { 3, 5 };
+    // littleEnigmaSet(le, password);
+    // littleEnigmaPrint(le);
+    // printf("\n\n");
+
+    // char* text = "TEXT";
+    // char* code = littleEnigmaEncrypt(le, text);
+    // littleEnigmaPrint(le);
+    // printf("%s -> %s\n\n", text, code);
+
+    // littleEnigmaSet(le, password);
+    // littleEnigmaPrint(le);
+    // printf("\n\n");
+
+    // char* decode = littleEnigmaDecrypt(le, code);
+    // littleEnigmaPrint(le);
+    // printf("%s -> %s -> %s\n\n", text, code, decode);
+
+    // if(code) free(code);
+    // if(decode) free(decode);
+
+    // littleEnigmaDelete(le);
+
+//Encriptar y desncriptar un mensaje de 0 caracteres
+    char* alphabetPermutation[1];
     alphabetPermutation[0] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
-    alphabetPermutation[1] = "NTZPSFBOKMWRCJDIVLAEYUXHGQ";
-    struct littleEnigma* le = littleEnigmaNew(alphabetPermutation, 2);
-    littleEnigmaPrint(le);
-    printf("\n\n");
-
-    int password[2] = { 3, 5 };
+      
+   
+    struct littleEnigma* le = littleEnigmaNew(alphabetPermutation, 5);
+    int password[3] = { 0 };
     littleEnigmaSet(le, password);
-    littleEnigmaPrint(le);
-    printf("\n\n");
 
-    char* text = "TEXT";
+    char* text = " ";
     char* code = littleEnigmaEncrypt(le, text);
-    littleEnigmaPrint(le);
-    printf("%s -> %s\n\n", text, code);
 
+    printf("Encriptar y desncriptar un mensaje de 0 caracteres:");
+    littleEnigmaPrint(le);
+    printf("Encripta un mensaje de 0 caracteres: %s -> %s\n\n", text, code);
+    
     littleEnigmaSet(le, password);
-    littleEnigmaPrint(le);
-    printf("\n\n");
-
+    
     char* decode = littleEnigmaDecrypt(le, code);
     littleEnigmaPrint(le);
-    printf("%s -> %s -> %s\n\n", text, code, decode);
+    printf("Desencripta el codigo: %s -> %s \n\n", text, decode);
+
 
     if(code) free(code);
     if(decode) free(decode);
-
     littleEnigmaDelete(le);
+   
 
+
+  //Encriptar y desncriptar un mensaje de 35 caracteres iguales con un solo disco. 
+  char* alphabetPermutation2[1];
+      alphabetPermutation2[0] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+   
+
+    struct littleEnigma* le2 = littleEnigmaNew(alphabetPermutation2, 5);
+    int password2[1] = { 0 };
+    littleEnigmaSet(le2, password2);
+
+    char* text2 = "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM";
+    char* code2 = littleEnigmaEncrypt(le2, text2);
+
+    printf("Encriptar y desncriptar un mensaje de 35 caracteres iguales con una maquina de un solo disco:");
+    littleEnigmaPrint(le2);
+    printf("Encripta un mensaje de 35 caracteres con una maquina de un solo disco: %s -> %s\n\n", text2, code2);
+    
+    littleEnigmaSet(le2, password2);
+    
+    char* decode2 = littleEnigmaDecrypt(le2, code2);
+    littleEnigmaPrint(le2);
+    printf("Desencripta el codigo con una maquina de un solo disco: %s -> %s\n\n", text2, decode2);
+
+
+    if(code2) free(code2);
+    if(decode2) free(decode2);
+    littleEnigmaDelete(le2);
+   
+
+
+
+
+  //Encriptar y desncriptar un mensaje de 10 caracteres con un 3 discos. 
+    char* alphabetPermutation3[3];
+      alphabetPermutation3[0] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+      alphabetPermutation3[1] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+      alphabetPermutation3[2] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+    
+
+    struct littleEnigma* le3 = littleEnigmaNew(alphabetPermutation3, 3);
+    int password3[3] = { 0, 0, 0 };
+    littleEnigmaSet(le3, password3);
+
+    char* text3 = "ALANTURING";
+    char* code3 = littleEnigmaEncrypt(le3, text3);
+
+    printf("Encriptar y desncriptar un mensaje de 10 caracteres con una maquina de un 3 discos:");
+    littleEnigmaPrint(le3);
+    printf("Encripta un mensaje de 10 caracteres con una maquina de un 3 discos: %s -> %s\n\n", text3, code3);
+    
+    littleEnigmaSet(le3, password3);
+    
+    char* decode3 = littleEnigmaDecrypt(le3, code3);
+    littleEnigmaPrint(le3);
+    printf("Desencripta el codigo con una maquina de 3 discos %s -> %s\n\n", text3, decode3);
+
+
+    if(code3) free(code3);
+    if(decode3) free(decode3);
+    littleEnigmaDelete(le3);
+   
+
+//Encriptar y desencriptar un mensaje de 10 caracteres con un 5 discos. 
+char* alphabetPermutation4[5];
+      alphabetPermutation4[0] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+      alphabetPermutation4[1] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+      alphabetPermutation4[2] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+      alphabetPermutation4[3] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+      alphabetPermutation4[4] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+
+    
+
+    struct littleEnigma* le4 = littleEnigmaNew(alphabetPermutation4, 5);
+    int password4[5] = { 0, 0, 0, 0, 0 };
+    littleEnigmaSet(le4, password4);
+
+    char* text4 = "NIELSBOHR";
+    char* code4 = littleEnigmaEncrypt(le4, text4);
+
+    printf("Encriptar y desncriptar un mensaje de 10 caracteres con una maquina de un 5 discos:");
+    littleEnigmaPrint(le4);
+    printf("Encripta un mensaje de 10 caracteres con una maquina de un 5 discos: %s -> %s\n\n", text4, code4);
+    
+    littleEnigmaSet(le4, password4);
+    
+    char* decode4 = littleEnigmaDecrypt(le4, code4);
+    littleEnigmaPrint(le4);
+    printf("Desencripta el codigo con una maquina de 5 discos %s -> %s\n\n", text4, decode4);
+
+
+    if(code4) free(code4);
+    if(decode4) free(decode4);
+    littleEnigmaDelete(le4);
+   
+
+
+   //Encriptar y desncriptar un mensaje de 10 caracteres con un 8 discos. 
+      char* alphabetPermutation5[8];
+      alphabetPermutation5[0] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+      alphabetPermutation5[1] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+      alphabetPermutation5[2] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+      alphabetPermutation5[3] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+      alphabetPermutation5[4] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+      alphabetPermutation5[5] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+      alphabetPermutation5[6] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+      alphabetPermutation5[7] = "JGDQOXUSCAMIFRVTPNEWKBLZYH";
+
+
+    struct littleEnigma* le5 = littleEnigmaNew(alphabetPermutation5, 8);
+    int password5[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+    littleEnigmaSet(le5, password5);
+
+    char* text5 = "MARIECURIE";
+    char* code5 = littleEnigmaEncrypt(le5, text5);
+
+    printf("Encriptar y desncriptar un mensaje de 10 caracteres con una maquina de un 8 discos:");
+    littleEnigmaPrint(le5);
+    printf("Encripta un mensaje de 10 caracteres con una maquina de un 5 discos: %s -> %s\n\n", text5, code5);
+    
+    littleEnigmaSet(le5, password5);
+    
+    char* decode5 = littleEnigmaDecrypt(le5, code5);
+    littleEnigmaPrint(le5);
+    printf("Desencripta el codigo con una maquina de 5 discos %s -> %s\n\n", text5, decode5);
+
+
+    if(code5) free(code5);
+    if(decode5) free(decode5);
+    littleEnigmaDelete(le5);
     // Liberar la memoria asignada a 'w1', 'w2' y 'w3'
     wheelDelete(w1);
     wheelDelete(w2);
@@ -103,7 +260,6 @@ int main(){
     wheelDelete(w4);
     wheelDelete(w5);
     wheelDelete(w6);
-    wheelDelete(w7);
 
     return 0;
 }
